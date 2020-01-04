19098472fef345b20b24478e6fb6d55e1ffdb0d3
Brad Streu <bstr77@gmail..com>
2020-01-03 22:00:01 -0500

    Initial Commit

---
+++ MVC_PWx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_PWx.Startup))]
+namespace MVC_PWx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
