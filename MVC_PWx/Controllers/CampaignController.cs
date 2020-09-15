00080f342a5f78fde679b0925fbb2a828c528973
Alex-Streu <astreu@cfl.rr.com>
2020-09-15 01:12:34 -0400

    draft images, image insertion

--- MVC_PWx/Controllers/CampaignController.cs
+++ MVC_PWx/Controllers/CampaignController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace MVC_PWx.Controllers
 {
+    [Authorize]
     [HasAccess(Priviledge = AppLogic.Priviledge.Player)]
     public class CampaignController : DeneirsController
     {
---
+++ MVC_PWx/Controllers/DungeonController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_PWx.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_PWx.Controllers
+{
+    [Authorize, HasCampaign, HasAccess(Priviledge = AppLogic.Priviledge.DM)]
+    public class DungeonController : DeneirsController
+    {
+        // GET: Dungeon
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return RedirectToAction("Edit", new { id = Guid.NewGuid() });
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            try
+            {
+                ViewBag.TileImages = Directory.EnumerateFiles(Server.MapPath("~/Content/img/dungeon tiles/"))
+                    .Select(x => "/Content/img/dungeon tiles/" + Path.GetFileName(x)).ToList();
+            }
+            catch (Exception ex) { }
+
+            return View();
+        }
+    }
+}
